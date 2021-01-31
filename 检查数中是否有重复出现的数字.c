010550762ac97d2580e76e424f664126b3d2bac4
JJ-Peter <1519108798@qq.com>
2021-01-31 17:35:54 +0800

    快速排序，布尔变量

--- 检查数中是否有重复出现的数字.c
+++ 检查数中是否有重复出现的数字.c
@@ -6,11 +6,39 @@
 
 #include<stdio.h>
 #include<stdbool.h>
+#define N 10
+
+int split(int a[],int low, int high)
+{
+	int part_element=a[low];
+	for(;;){
+		while(low<high&&part_element<=a[high])
+		high--;
+		if(low>=high) break;
+		a[low++]=a[high];
+		while(low<high&&a[low]<=part_element)
+			low++;
+		if(low>=high) break;
+		a[high--]=a[low];
+	} 
+	a[high]=part_element;
+	return high;
+}
+
+void quicksort(int a[],int low,int high)
+{
+	int middle;
+	if(low>=high) 
+		return;
+	middle = split(a,low,high);
+	quicksort(a,low,middle-1);
+	quicksort(a,middle+1,high);
+}
 
 int main(void)
 {
-	bool digit_seen[10]={false};
-	int digit;
+	bool digit_seen[10]={false},digit_print[10]={false};
+	int a[N]={0},digit,i,t=0;
 	long n;
 	 
 	printf("Enter a number: ");
@@ -26,7 +54,25 @@ int main(void)
 	}
 		
 	if(n>0){
-		printf("Repeated digit\n");
+		printf("Repeated digit: ");
+		while(n>0){
+			digit=n%10;
+			if(digit_seen[digit]){
+				digit_print[digit]=true;	
+			}
+			digit_seen[digit]=true;
+			n/=10;
+		}
+		for(i=0;i<10;i++){
+			if(digit_print[i]){
+				a[t++]=i;
+			}
+		}
+		quicksort(a,0,t-1);
+		for(i=0;i<t-1;i++){
+			printf("%d,",a[i]);
+		}
+		printf("%d.\n",a[t-1]);
 	}
 	else{
 			printf("No repeated digit\n");
